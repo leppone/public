b1fb0afbd927b062843da8563e1d9c7db78a64bb
Markus Lehtonen <markus.o.lehtonen@gmail.com>
2020-08-12 15:49:06 +0300

    Rebased different exercises into separate js-files and index.js now creates a navi-page (most likely not according to any standards)

--- fullstackopen/part1/src/index.js
+++ fullstackopen/part1/src/index.js
@@ -1,60 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Unicafe from './d_unicafe'
+import Kurssitiedot from './a_kurssitiedot'
 
-const Header = (props) => {
-  return (
-    <h1>{props.name}</h1>
-  )
-}
-
-const Content = (props) => {
-  return (
-    <>
-      <Part part={props.parts[0].name} exercise={props.parts[0].exercises} />
-      <Part part={props.parts[1].name} exercise={props.parts[1].exercises} />
-      <Part part={props.parts[2].name} exercise={props.parts[2].exercises} />
-    </>
-  )
-}
-
-const Part = (props) => {
-  return (
-    <p>
-      {props.part} {props.exercise}
-    </p>
-  )
-}
-
-const Total = (props) => {
-  return (
-    <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
-  )
-}
+const Button = (props) => (
+  <button onClick={props.handleClick}>{props.text}</button>
+)
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
 
   return (
     <div>
-      <Header name={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
+      <h1>Select app:</h1>
+      <Button handleClick={() => ReactDOM.render(<Kurssitiedot />, document.getElementById('root'))} text="a_kurssitiedot" />
+      <Button handleClick={() => ReactDOM.render(<Unicafe />, document.getElementById('root'))} text="d_unicafe" />
     </div>
   )
 }
