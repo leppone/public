8cefcc0dec4e572685c54a4e1f314f819f1af82a
Markus Lehtonen <markus.o.lehtonen@gmail.com>
2020-08-13 21:52:36 +0300

    anecdote example finished

--- fullstackopen/part1/src/components/d_anekdootit.js
+++ fullstackopen/part1/src/components/d_anekdootit.js
@@ -1,15 +1,37 @@
 import React, { useState } from 'react'
 
+const Button = (props) => (
+  <button onClick={props.handleClick}>{props.text}</button>
+)
+
 const Anekdootit = ({anecdotes}) => {
   const [selected, setSelected] = useState(0)
+  const [votes, vote] = useState(new Array(anecdotes.length + 1).join('0').split('').map(parseFloat))
+  const [highest, setHighest] = useState(0)
 
-  console.log(selected)
+  const handleVote = () => {
+    const copy = [...votes]
+    copy[selected] += 1
+    if( copy[selected] > copy[highest] )
+      setHighest(selected)
+    vote(copy)
+    console.log(votes, "highest", highest)
+  }
+
+  console.log(votes)
 
   return (
     <div>
-      {anecdotes[selected]}
-      <br/>
-      <button onClick={() => setSelected(Math.floor((Math.random() * anecdotes.length )))}>next anecdote</button>
+      <h1>Anecdote of the day</h1>
+      {anecdotes[selected]} <br/>
+      has {votes[selected]} votes <br/>
+      <Button handleClick={handleVote} text="vote" />
+      <Button handleClick={ () => 
+        setSelected(Math.floor((Math.random() * anecdotes.length )))} 
+        text="next anecdote" />
+      <h1>Anecdote with most votes</h1>
+      {anecdotes[highest]} <br/>
+      has {votes[highest]} votes <br/>
     </div>
   )
 }
