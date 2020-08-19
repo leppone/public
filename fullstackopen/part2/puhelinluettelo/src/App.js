c9e21a39eb24f304c40ba9e91c8855e4e8a2d39a
Markus Lehtonen <markus.o.lehtonen@gmail.com>
2020-08-19 15:29:33 +0300

    puhelinluettelo implemented

--- fullstackopen/part2/puhelinluettelo/src/App.js
+++ fullstackopen/part2/puhelinluettelo/src/App.js
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+const App = () => {
+  // State handlers
+  const [ persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
+  const [ filter, setFilter ] = useState('')
+
+  // Internal functions
+  function addName(event) {
+    event.preventDefault()
+
+    console.log("name to be added: ", newName)
+    var person = persons.find(p => console.log("existing item : ", p.name) || p.name.toLowerCase() === newName.toLowerCase())
+    console.log("found entry: ", person)
+
+    if( person !== undefined ) {
+      window.alert(`${newName} is already added to phonebook`)
+      return
+    }
+    
+    const nameObject = {
+      name: newName,
+      number: newNumber
+    }
+    
+    setPersons(persons.concat(nameObject))
+    setNewName('')
+    setNewNumber('')
+  }
+
+
+  // Event handlers
+  const handleNameAdd = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const handleNumberAdd = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+  const handleFilter = (event) => {
+    setFilter(event.target.value)
+  }
+
+  // "Main"
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Phonebook</h2>
+      <div>filter shown with <input onChange={handleFilter} /></div>
+      <form onSubmit={addName}>
+          <div>name: <input value={newName} onChange={handleNameAdd} /></div>
+          <div>number: <input value={newNumber} onChange={handleNumberAdd} /></div>
+          <button type="submit">add</button>
+      </form>
+      <h2>Numbers</h2>
+      {persons
+        .filter(person => person.name.toLowerCase().indexOf(filter.toLowerCase()) > -1 )
+        .map( person => <p key={person.name}>{person.name} {person.number}</p>)}
     </div>
-  );
+  )
+
 }
 
-export default App;
+export default App
--- fullstackopen/part2/puhelinluettelo/src/index.js
+++ fullstackopen/part2/puhelinluettelo/src/index.js
@@ -1,27 +1,17 @@
-import React, { useState } from 'react'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-const App = () => {
-  const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
-  const [ newName, setNewName ] = useState('')
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-  return (
-    <div>
-      <h2>Phonebook</h2>
-      <form>
-        <div>
-          name: <input />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      ...
-    </div>
-  )
-
-}
-
-export default App
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
