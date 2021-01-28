d52978ebc9bbc605565d2872b991f4e78a18eeaa
Markus Lehtonen <markus.o.lehtonen@gmail.com>
2021-01-28 13:42:42 +0200

    paging disabled when only one page

--- scoreboard/src/components/PagingBar.js
+++ scoreboard/src/components/PagingBar.js
@@ -2,36 +2,41 @@ import React, { useState } from 'react';
 import Pagination from 'react-bootstrap/Pagination'
 
 const PagingBar = ({
-    // Params
-    // {scores} : score container
-    // {itemsPerPage} : items to show on one page
-    // {setFirstIndex} : first item index on page
-    scores,
-    itemsPerPage,
-    setFirstIndex
+  // Params
+  // {scores} : score container
+  // {itemsPerPage} : items to show on one page
+  // {setFirstIndex} : first item index on page
+  scores,
+  itemsPerPage,
+  setFirstIndex
 }) => {
 
-    // --- States ---
-    const [ active, setActive] = useState(1);
+  // --- States ---
+  const [ active, setActive] = useState(1);
 
 
-    // --- Internal functions ---
-    const handleActive = (num) => {
-        setActive(num);
-        setFirstIndex(num*itemsPerPage-itemsPerPage);
-    }
+  // --- Internal functions ---
+  const handleActive = (num) => {
+      setActive(num);
+      setFirstIndex(num*itemsPerPage-itemsPerPage);
+  }
 
 
-    // --- Component content ---  
-    let items = [];
-    for (let num = 1; num <= Math.ceil(scores.length/itemsPerPage); num++) {
-      items.push(
-        <Pagination.Item key={num} active={num === active} onClick={() => handleActive(num)}>
-          {num}
-        </Pagination.Item>
-      );
-    } 
+  // --- Component content ---  
+  let items = [];
+  for (let num = 1; num <= Math.ceil(scores.length/itemsPerPage); num++) {
+    items.push(
+      <Pagination.Item key={num} active={num === active} onClick={() => handleActive(num)}>
+        {num}
+      </Pagination.Item>
+    );
+  } 
 
+  if(items.length <= 1) {
+    return null;
+  }
+  else {
+      
     return (
         <div>
             <Pagination>
@@ -40,6 +45,7 @@ const PagingBar = ({
             <br />
         </div>
     );
+  }
 }
 
 export default PagingBar;
